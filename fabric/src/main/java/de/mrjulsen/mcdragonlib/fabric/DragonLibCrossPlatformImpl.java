28b17ceab85fb297853f3c205dd49af23a4ebdb9
MrJulsen <geisterdrache02@gmail.com>
2024-10-26 23:30:50 +0200

    Fixed config

--- fabric/src/main/java/de/mrjulsen/mcdragonlib/fabric/DragonLibCrossPlatformImpl.java
+++ fabric/src/main/java/de/mrjulsen/mcdragonlib/fabric/DragonLibCrossPlatformImpl.java
@@ -1,14 +1,14 @@
 package de.mrjulsen.mcdragonlib.fabric;
 
-import net.minecraftforge.api.ModLoadingContext;
 import net.minecraftforge.fml.config.ModConfig;
 
 import de.mrjulsen.mcdragonlib.DragonLib;
 import de.mrjulsen.mcdragonlib.config.ModCommonConfig;
+import fuzs.forgeconfigapiport.impl.config.ForgeConfigRegistryImpl;
 
 public class DragonLibCrossPlatformImpl {
 
     public static void registerConfig() {
-        ModLoadingContext.registerConfig(DragonLib.MODID, ModConfig.Type.COMMON, ModCommonConfig.SPEC, DragonLib.MODID + "-common.toml");
+        ForgeConfigRegistryImpl.INSTANCE.register(DragonLib.MODID, ModConfig.Type.COMMON, ModCommonConfig.SPEC, DragonLib.MODID + "-common.toml");
     }
 }
