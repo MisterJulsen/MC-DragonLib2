7b3cbd517c9b7f9c52475a5958df8f79125af825
MrJulsen <geisterdrache02@gmail.com>
2025-01-03 17:03:02 +0100

    Updated to 1.19.2

--- common/src/main/java/de/mrjulsen/mcdragonlib/compat/CompatManager.java
+++ common/src/main/java/de/mrjulsen/mcdragonlib/compat/CompatManager.java
@@ -16,7 +16,7 @@ public final class CompatManager {
     private static record Mod(String name, String id, String url) {}
 
     public static final Mod SODIUM = new Mod("Sodium", "sodium", "https://modrinth.com/mod/sodium");
-    public static final Mod UNOFFICIAL_SODIUM_BIOME_BLENDING_FIX = new Mod("Unofficial Sodium Biome Blending Fix", "unofficial_sodium_biome_blending_fix", "https://modrinth.com/mod/unofficial-sodium-biome-blending-fix");
+    public static final Mod SODIUM_BLENDING_REGISTRY = new Mod("Sodium Blending Registry", "sodium-blendingregistry", "https://modrinth.com/mod/sodium-blendingregistry");
 
     private static boolean sendSodiumWarn = false;
 
@@ -27,7 +27,7 @@ public final class CompatManager {
         ClientPlayerEvent.CLIENT_PLAYER_JOIN.register((player) -> {
             // SODIUM
             if (sendSodiumWarn && !ModCommonConfig.HIDE_SODIUM_WARNING.get() && Platform.isFabric() &&
-                Platform.isModLoaded(SODIUM.id()) && !Platform.isModLoaded(UNOFFICIAL_SODIUM_BIOME_BLENDING_FIX.id())
+                Platform.isModLoaded(SODIUM.id()) && !Platform.isModLoaded(SODIUM_BLENDING_REGISTRY.id())
             ) {
                 sendSodiumWarning(player);
             }
@@ -48,10 +48,10 @@ public final class CompatManager {
             .append(TextUtils.text("] ").withStyle(ChatFormatting.DARK_GRAY))
             .append(TextUtils.translate("compat." + DragonLib.MODID + ".sodium_warn",
                 TextUtils.text(SODIUM.name()),
-                TextUtils.text(UNOFFICIAL_SODIUM_BIOME_BLENDING_FIX.name()).withStyle(x -> x
+                TextUtils.text(SODIUM_BLENDING_REGISTRY.name()).withStyle(x -> x
                     .withColor(ChatFormatting.BLUE)
                     .withUnderlined(true)
-                    .withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, UNOFFICIAL_SODIUM_BIOME_BLENDING_FIX.url()))
+                    .withClickEvent(new ClickEvent(ClickEvent.Action.OPEN_URL, SODIUM_BLENDING_REGISTRY.url()))
                     .withHoverEvent(new HoverEvent(HoverEvent.Action.SHOW_TEXT, TextUtils.translate("compat." + DragonLib.MODID + ".click_to_open_modpage")))
                 )
             ).withStyle(ChatFormatting.YELLOW))
