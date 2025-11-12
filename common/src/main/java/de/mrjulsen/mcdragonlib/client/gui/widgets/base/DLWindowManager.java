c77895301b4ac5588db8bd0ade489a5711a55a59
MrJulsen <geisterdrache02@gmail.com>
2025-11-12 21:34:30 +0100

    Fixed crash

--- common/src/main/java/de/mrjulsen/mcdragonlib/client/gui/widgets/base/DLWindowManager.java
+++ common/src/main/java/de/mrjulsen/mcdragonlib/client/gui/widgets/base/DLWindowManager.java
@@ -323,7 +323,7 @@ public class DLWindowManager implements IEventDispatcher<DLWindowManager>, MenuA
     }
 
     public boolean isPauseScreen() {
-        return this.isPauseScreen || getFocusedWindow().pauseGame.get();
+        return this.isPauseScreen || (getFocusedWindow() != null && getFocusedWindow().pauseGame.get());
     }
 
     public void setPauseScreen(boolean b) {
