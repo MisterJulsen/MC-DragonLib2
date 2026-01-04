8d47f88654e55756fb6b1fc7d21d66d19c5a4b3c
MrJulsen <geisterdrache02@gmail.com>
2026-01-04 17:39:28 +0100

    Server config registry

--- forge/src/main/java/de/mrjulsen/mcdragonlib/forge/DragonLibCrossPlatformImpl.java
+++ forge/src/main/java/de/mrjulsen/mcdragonlib/forge/DragonLibCrossPlatformImpl.java
@@ -4,10 +4,12 @@ import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.config.ModConfig;
 import de.mrjulsen.mcdragonlib.DragonLib;
 import de.mrjulsen.mcdragonlib.config.ModCommonConfig;
+import de.mrjulsen.mcdragonlib.config.ModServerConfig;
 
 public class DragonLibCrossPlatformImpl {
 
     public static void registerConfig() {
         ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, ModCommonConfig.SPEC, DragonLib.MODID + "-common.toml");
+        ModLoadingContext.get().registerConfig(ModConfig.Type.SERVER, ModServerConfig.SPEC, DragonLib.MODID + "-server.toml");
     }
 }
